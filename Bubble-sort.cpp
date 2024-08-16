099952dd27c238c66f4e40d4aa3eaaf74b3ff6a6
quangcoder2k5 <2351010170quang@ou.edu.vn>
2024-08-16 18:16:46 +0700

    Update Bubble-sort.cpp

--- Bubble-sort.cpp
+++ Bubble-sort.cpp
@@ -1,1 +1,20 @@
+#include <iostream>
+using namespace std;
 
+#define MAX 100
+
+void Bubble_Sort (int a[], int n) {
+    for (int i = 0; i < n -1;i++) {
+        for (int j = n - 1; j > i; j--) {
+        if(a[j - 1] > a[j]) {
+            swap(a[j-1],a[i]);
+             }
+        }
+    }
+}
+
+int main() {
+    int a[MAX];
+    int n;
+    Bubble_Sort(a,n);
+}
