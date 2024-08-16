dea2dafaeb5adbebca20883e7395711720414736
quangcoder2k5 <2351010170quang@ou.edu.vn>
2024-08-16 18:35:59 +0700

    Update Interchange-sort.cpp

--- Interchange-sort.cpp
+++ Interchange-sort.cpp
@@ -1,1 +1,15 @@
+#include <iostream>
+using namespace std;
 
+#define MAX 100
+void Interchange (int a[],int n) {
+  for (int i = 0; i < n - 1; i++) 
+    for (int j = i + 1; j < n; j++)
+      if(a[i] > a[j] {
+        swap(a[i],a[j]);
+      }
+}  
+int main() {
+  int a[MAX];
+  int n;
+}
