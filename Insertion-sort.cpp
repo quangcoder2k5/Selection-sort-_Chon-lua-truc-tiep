7299491da4f952e92f8b581bccf04a3a5043f5a3
quangcoder2k5 <2351010170quang@ou.edu.vn>
2024-08-16 18:03:19 +0700

    Update Insertion-sort.cpp

--- Insertion-sort.cpp
+++ Insertion-sort.cpp
@@ -1,1 +1,24 @@
 
+#include <iostream>
+using namespace std;
+
+#define MAX 100
+
+void InsertSort (int a[], int n) {
+	int x,j;
+	for (int i = 1; i < n; i++) {
+		x = a[i];
+		j = i - 1;
+		while (0 <= j && x < a[j]) {
+			a[j + 1] = a[j];
+			j--;
+		}
+		a[j + 1] = x;
+	}
+}
+
+int main() {
+	int a[MAX];
+	int n;
+	InsertSort(a,n);
+}
